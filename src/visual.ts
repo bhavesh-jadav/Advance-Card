c6e7a04f09d37579e64223418d10a4c6174e5716
bhavesh-jadav <bhaveshhjadav@gmail.com>
2018-06-15 12:20:35 +0530

    v1.0.5

--- src/visual.ts
+++ src/visual.ts
@@ -25,7 +25,7 @@
  */
 
 import valueFormatter = powerbi.extensibility.utils.formatting.valueFormatter;
-let version = "1.0.4";
+let version = "1.0.5";
 let helpUrl = "https://github.com/bhavesh-jadav/Advance-Card/wiki";
 module powerbi.extensibility.visual {
     "use strict";
